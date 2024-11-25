00d9f0fa264c0a2ad49790e777f94846dee2668e
Francisco Lopez <lopezf28@gmail.com>
2024-11-25 11:51:33 -0300

    configurando vista

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tasty-foods/', 
+  base: '/tasty-foods/', // Este debe ser el nombre exacto de tu repositorio
   plugins: [react()],
 });
-
